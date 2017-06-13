eb8de4d00d8c1d494c22e5f5220a37fdfe166554
Gerhard Nachtmann <kpm.nachtmann@gmail.com>
2017-06-13 23:39:27 +0200

    reroxygenized

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -15,3 +15,13 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+
+static const R_CallMethodDef CallEntries[] = {
+    {"firstpackage_rcpp_hello", (DL_FUNC) &firstpackage_rcpp_hello, 0},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_firstpackage(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
